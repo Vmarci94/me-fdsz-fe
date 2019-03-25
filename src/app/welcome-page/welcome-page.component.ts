923e8ab2aeea8a56d9587fab1170e275b055e843
varga.marton <varga@jegymester.hu>
2019-03-25 21:32:58 +0100

    video background step 1

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,9 +9,22 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
-  constructor() {
+  constructor(private snackBar: MatSnackBar, private router: Router) {
   }
 
   ngOnInit() {
   }
+
+  login() {
+    if (localStorage.getItem('isLoggedin')) {
+      this.snackBar.open('Ön már be van jelentkezve XYZ!', 'kijelentkezés', {
+        duration: 3000,
+      }).afterDismissed().subscribe(next => {
+        this.router.navigate(['/dashboard']);
+      });
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
 }
--- src/app/welcome-page/welcome-page.module.ts
+++ src/app/welcome-page/welcome-page.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {TranslateModule} from '@ngx-translate/core';
 import {WelcomePageComponent} from './welcome-page.component';
 import {WelcomePageRoutingModule} from './welcome-page-routing.module';
+import {MatSnackBarModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
     WelcomePageRoutingModule,
+    MatSnackBarModule
   ],
   declarations: [WelcomePageComponent]
 })
