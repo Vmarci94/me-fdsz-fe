7096a32c3edd2a1fe82a8e1ec6988e3ab038821d
Vmarci94 <vmarci94@gmail.com>
2019-04-15 15:46:13 +0200

    add modal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SignupComponent} from '../signup-modal/signup.component';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {Router} from '@angular/router';
@@ -12,26 +12,34 @@ import {MatSnackBar} from '@angular/material';
 export class HomeComponent implements OnInit {
 
   constructor(
-    // private modalService: NgbModal,
-    // private router: Router,
+    private modalService: NgbModal,
+    private router: Router,
     // private snackBar: MatSnackBar
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
   }
 
-  // signup() {
-  //   this.modalService.open(SignupComponent);
-  //
-  //   if (localStorage.getItem('isLoggedin')) {
-  //     this.snackBar.open('Ön már be van jelentkezve XYZ!', 'kijelentkezés', {
-  //       duration: 3000,
-  //     }).afterDismissed().subscribe(next => {
-  //       this.router.navigate(['/dashboard']);
-  //     });
-  //   } else {
-  //     this.router.navigate(['/signup']);
-  //   }
-  // }
+  // bejelentkezés
+  signin() {
+
+  }
+
+
+  // regisztrácio
+  signup() {
+    this.modalService.open(SignupComponent);
+    //
+    //   if (localStorage.getItem('isLoggedin')) {
+    //     this.snackBar.open('Ön már be van jelentkezve XYZ!', 'kijelentkezés', {
+    //       duration: 3000,
+    //     }).afterDismissed().subscribe(next => {
+    //       this.router.navigate(['/dashboard']);
+    //     });
+    //   } else {
+    //     this.router.navigate(['/signup']);
+    //   }
+  }
 
 }
