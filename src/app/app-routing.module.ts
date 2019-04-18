63994dc4e34c472c4662ad3477e89c7fac406bee
Vmarci94 <vmarci94@gmail.com>
2019-04-18 14:58:23 +0200

    in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainAppComponent} from './components/main-app/main-app.component';
 import {HomeComponent} from './components/home/home.component';
+import {FeedPageComponent} from './components/feed-page/feed-page.component';
 
 const routes: Routes = [];
 
@@ -11,7 +12,8 @@ const routes: Routes = [];
     component: MainAppComponent,
     children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'feeds', component: FeedPageComponent}
     ]
   }])],
   exports: [RouterModule]
