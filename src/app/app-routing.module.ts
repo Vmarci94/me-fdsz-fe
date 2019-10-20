af2993ccc0e1c95290bd938a542caedba89831ea
Vmarci94 <vmarci94@gmail.com>
2019-10-20 16:35:36 +0200

    css clean

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {FeedPageComponent} from './components/feed-page/feed-page.component';
-import {AuthGuardService as AuthGuard} from './services/auth-guard.service';
 import {PersonalComponent} from './components/personal/personal.component';
 import {ResortPageComponent} from './components/resort-page/resort-page.component';
 import {AdminPageComponent} from './components/admin-page/admin-page.component';
