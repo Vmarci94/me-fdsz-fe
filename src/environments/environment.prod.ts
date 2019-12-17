0b5f4293592e859a1f205317cb92d4e3019accf2
Vmarci94 <vmarci94@gmail.com>
2019-12-17 02:20:56 +0100

    in progress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: false,
-  connectionURL: '/api',
+  connectionURL: 'http://backend:8081/api',
   header: new HttpHeaders({'Content-Type': 'application/json'})
 };
